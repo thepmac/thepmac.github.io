15d9c148744436cd5f17b764161f97045a39f112
Patrick M <patrickymacdonald@gmail.com>
2019-12-13 02:54:57 -0500

    Update tooltips.js

--- js/city/tooltips.js
+++ js/city/tooltips.js
@@ -1,57 +1,12 @@
-function onHomeShelf(evt, text) {
-  let tooltip = document.getElementById("HomeShelf");
+function onToolTip(evt, text) {
+  let tooltip = document.getElementById("ToolTip");
   tooltip.innerHTML = text;
   tooltip.style.display = "block";
   tooltip.style.left = evt.pageX + 10 + 'px';
   tooltip.style.top = evt.pageY + 10 + 'px';
 }
 
-function offHomeShelf() {
-  var tooltip = document.getElementById("HomeShelf");
-  tooltip.style.display = "none";
-}
-
-//———————————————————————————————————//
-
-function onHomeBench(evt, text) {
-  let tooltip = document.getElementById("HomeBench");
-  tooltip.innerHTML = text;
-  tooltip.style.display = "block";
-  tooltip.style.left = evt.pageX + 10 + 'px';
-  tooltip.style.top = evt.pageY + 10 + 'px';
-}
-
-function offHomeBench() {
-  var tooltip = document.getElementById("HomeBench");
-  tooltip.style.display = "none";
-}
-
-//———————————————————————————————————//
-
-function onHomePlant(evt, text) {
-  let tooltip = document.getElementById("HomePlant");
-  tooltip.innerHTML = text;
-  tooltip.style.display = "block";
-  tooltip.style.left = evt.pageX + 10 + 'px';
-  tooltip.style.top = evt.pageY + 10 + 'px';
-}
-
-function offHomePlant() {
-  var tooltip = document.getElementById("HomePlant");
-  tooltip.style.display = "none";
-}
-
-//———————————————————————————————————//
-
-function onHomeShoeRack(evt, text) {
-  let tooltip = document.getElementById("HomeShoeRack");
-  tooltip.innerHTML = text;
-  tooltip.style.display = "block";
-  tooltip.style.left = evt.pageX + 10 + 'px';
-  tooltip.style.top = evt.pageY + 10 + 'px';
-}
-
-function offHomeShoeRack() {
-  var tooltip = document.getElementById("HomeShoeRack");
+function offToolTip() {
+  var tooltip = document.getElementById("ToolTip");
   tooltip.style.display = "none";
 }
