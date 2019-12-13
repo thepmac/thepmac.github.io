9b786c10eb5225dda66313cd73d8f7baa5dd9d61
Patrick M <patrickymacdonald@gmail.com>
2019-12-12 23:20:57 -0500

    Update tooltips.js

--- js/city/tooltips.js
+++ js/city/tooltips.js
@@ -1,5 +1,18 @@
+function onHomeShelf(evt, text) {
+  let tooltip = document.getElementById("HomeShelf");
+  tooltip.innerHTML = text;
+  tooltip.style.display = "block";
+  tooltip.style.left = evt.pageX + 10 + 'px';
+  tooltip.style.top = evt.pageY + 10 + 'px';
+}
+
+function offHomeShelf() {
+  var tooltip = document.getElementById("HomeShelf");
+  tooltip.style.display = "none";
+}
+
 function showTooltip(evt, text) {
-  let tooltip = document.getElementById("tt-shelf1");
+  let tooltip = document.getElementById("HomeBench");
   tooltip.innerHTML = text;
   tooltip.style.display = "block";
   tooltip.style.left = evt.pageX + 10 + 'px';
@@ -7,6 +20,6 @@ function showTooltip(evt, text) {
 }
 
 function hideTooltip() {
-  var tooltip = document.getElementById("tt-shelf1");
+  var tooltip = document.getElementById("HomeBench");
   tooltip.style.display = "none";
 }
