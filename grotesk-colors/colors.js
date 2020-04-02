7ba6f5ea7ba4c4ffa3985876a51b4f295dece0cb
Patrick M <patrickymacdonald@gmail.com>
2020-04-02 02:14:16 -0700

    Create colors.js

---
+++ grotesk-colors/colors.js
@@ -0,0 +1,79 @@
+let root = document.documentElement;
+
+function random_color_0() {
+  var x0 = Math.floor(Math.random() * 256);
+  var y0 = Math.floor(Math.random() * 256);
+  var z0 = Math.floor(Math.random() * 256);
+  var bgColor = "rgb(" + x0 + "," + y0 + "," + z0 + ")";
+  console.log(bgColor);
+  document.getElementById("c0r").innerHTML = x0;
+  document.getElementById("c0g").innerHTML = y0;
+  document.getElementById("c0b").innerHTML = z0;
+  root.style.setProperty("--color0", bgColor0);
+}
+random_color_0();
+
+function random_color_1() {
+  var x1 = Math.floor(Math.random() * 256);
+  var y1 = Math.floor(Math.random() * 256);
+  var z1 = Math.floor(Math.random() * 256);
+  var bgColor1 = "rgb(" + x1 + "," + y1 + "," + z1 + ")";
+  console.log(bgColor1);
+  document.getElementById("c1r").innerHTML = x1;
+  document.getElementById("c1g").innerHTML = y1;
+  document.getElementById("c1b").innerHTML = z1;
+  root.style.setProperty("--color1", bgColor1);
+}
+random_color_1();
+
+function random_color_2() {
+  var x2 = Math.floor(Math.random() * 256);
+  var y2 = Math.floor(Math.random() * 256);
+  var z2 = Math.floor(Math.random() * 256);
+  var bgColor2 = "rgb(" + x2 + "," + y2 + "," + z2 + ")";
+  console.log(bgColor2);
+  document.getElementById("c2r").innerHTML = x2;
+  document.getElementById("c2g").innerHTML = y2;
+  document.getElementById("c2b").innerHTML = z2;
+  root.style.setProperty("--color2", bgColor2);
+}
+random_color_2();
+
+function random_color_3() {
+  var x3 = Math.floor(Math.random() * 256);
+  var y3 = Math.floor(Math.random() * 256);
+  var z3 = Math.floor(Math.random() * 256);
+  var bgColor3 = "rgb(" + x3 + "," + y3 + "," + z3 + ")";
+  console.log(bgColor3);
+  document.getElementById("c3r").innerHTML = x3;
+  document.getElementById("c3g").innerHTML = y3;
+  document.getElementById("c3b").innerHTML = z3;
+  root.style.setProperty("--color3", bgColor3);
+}
+random_color_3();
+
+function random_color_4() {
+  var x4 = Math.floor(Math.random() * 256);
+  var y4 = Math.floor(Math.random() * 256);
+  var z4 = Math.floor(Math.random() * 256);
+  var bgColor4 = "rgb(" + x4 + "," + y4 + "," + z4 + ")";
+  console.log(bgColor4);
+  document.getElementById("c4r").innerHTML = x4;
+  document.getElementById("c4g").innerHTML = y4;
+  document.getElementById("c4b").innerHTML = z4;
+  root.style.setProperty("--color4", bgColor4);
+}
+random_color_4();
+
+function random_color_5() {
+  var x5 = Math.floor(Math.random() * 256);
+  var y5 = Math.floor(Math.random() * 256);
+  var z5 = Math.floor(Math.random() * 256);
+  var bgColor5 = "rgb(" + x5 + "," + y5 + "," + z5 + ")";
+  console.log(bgColor5);
+  document.getElementById("c5r").innerHTML = x5;
+  document.getElementById("c5g").innerHTML = y5;
+  document.getElementById("c5b").innerHTML = z5;
+  root.style.setProperty("--color5", bgColor5);
+}
+random_color_5();
