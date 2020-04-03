d15969612d1575c4250b20376858341e7a5d1332
Patrick M <patrickymacdonald@gmail.com>
2020-04-02 17:49:08 -0700

    Create header-scroll.js

---
+++ korean-dmz/scripts/header-scroll.js
@@ -0,0 +1,37 @@
+// window.onscroll = function() {scrollFunction()};
+
+function scrollHeader2() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("c2header").style.fontSize = "28px";
+  } else {
+    document.getElementById("c2header").style.fontSize = "96px";
+  }
+
+function scrollHeader3() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("c3header").style.fontSize = "28px";
+  } else {
+    document.getElementById("c3header").style.fontSize = "96px";
+  }
+
+function scrollHeader4() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("c4header").style.fontSize = "28px";
+  } else {
+    document.getElementById("c4header").style.fontSize = "96px";
+  }
+
+function scrollHeader5() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("c5header").style.fontSize = "28px";
+  } else {
+    document.getElementById("c5header").style.fontSize = "96px";
+  }
+
+function scrollHeader6() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("c6header").style.fontSize = "28px";
+  } else {
+    document.getElementById("c6header").style.fontSize = "96px";
+  }
+}
