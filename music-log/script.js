167450cbbfdbb51409d52f6e9797452ae86dced5
Patrick M <patrickymacdonald@gmail.com>
2022-07-18 16:40:57 -0400

    loggggggg

--- music-log/script.js
+++ music-log/script.js
@@ -1,5 +1,32 @@
 let database = {
     "entries": [{
+            "entry": 5,
+            "date": "2022-07-17 SUN",
+            "title": "In Vitro - Volume One",
+            "artist": "Claro Intelecto",
+            "year": 2019,
+            "type": "album, anthology",
+            "cover": "https://f4.bcbits.com/img/a2487780159_10.jpg"
+        },
+        {
+            "entry": 4,
+            "date": "2022-07-17 SUN",
+            "title": "[Las Vegas]",
+            "artist": "Burger / Ink",
+            "year": 1998,
+            "type": "album",
+            "cover": "https://www.electro-space.de/wp-content/uploads/2022/03/Burger-Ink-Las-Vegas.jpg"
+        },
+        {
+            "entry": 3,
+            "date": "2022-07-17 SUN",
+            "title": "Connecting The Dots",
+            "artist": "Michael Mayer",
+            "year": 2021,
+            "type": "album",
+            "cover": "https://f4.bcbits.com/img/a0608671658_10.jpg"
+        },
+        {
             "entry": 2,
             "date": "2022-07-17 SUN",
             "title": "Spiro World",
@@ -29,6 +56,10 @@ let database = {
     ]
 }
 
+
+
+
+
 function populateEntries() {
 
     document.getElementById("list").innerHTML += `<div class="entry" id="key"><div class="entryid">#</div><div class="cover">Cover</div><div class="title">Title</div><div class="artist">Artist</div><div class="year">Year</div><div class="type">Type</div><div class="date">Heard On</div></div>`
@@ -51,13 +82,3 @@ function populateEntries() {
 }
 
 populateEntries();
-
-
-
-/*
-function createEntry() {
-    document.getElementById("list").innerHTML += `<a href="#">Link to This Page</a><br>`
-}
-
-createEntry();
-*/
